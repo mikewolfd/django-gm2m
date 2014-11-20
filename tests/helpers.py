796b8d0f7741bba73acb35f9b41247ea22227c93
Thomas Khyn <thomas@ksytek.com>
2014-11-21 01:06:28 +1300

    v0.1.1

---
+++ tests/helpers.py
@@ -0,0 +1,32 @@
+import sys
+
+__test__ = False
+
+
+def app_mod_path(m):
+    return 'tests.' + m
+
+
+def del_app_models(app, app_module=False):
+    """
+    Unloads an app and its models module, as well as the app module
+    (optionally)
+    """
+    from .compat import cache_models
+
+    try:
+        del cache_models[app]
+    except KeyError:
+        pass
+
+    path = app_mod_path(app)
+
+    to_del = [path + '.models']
+    if app_module:
+        to_del.append(path)
+
+    for m in to_del:
+        try:
+            del sys.modules[m]
+        except KeyError:
+            pass
