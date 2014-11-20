31850f0565ace7afb614d0f1a3cd0a232f52ee20
Thomas Khyn <thomas@ksytek.com>
2014-11-20 23:48:46 +1300

    Django 1.7 is now the official supported version
    However, Django raises an exception when deep-copying related fields in the
    case of model inheritance.

---
+++ tests/helpers.py
@@ -0,0 +1,32 @@
+import sys
+
+from .compat import cache_models
+
+__test__ = False
+
+
+def app_mod_path(m):
+    return 'tests.' + m
+
+
+def del_app_models(app, app_module=False):
+    """
+    Unloads an app and its models module, as well as the app module
+    (optionally)
+    """
+    try:
+        del cache_models[app]
+    except KeyError:
+        pass
+
+    path = app_mod_path(app)
+
+    to_del = [path + '.models']
+    if app_module:
+        to_del.append(path)
+
+    for m in to_del:
+        try:
+            del sys.modules[m]
+        except KeyError:
+            pass
